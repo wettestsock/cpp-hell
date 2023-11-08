bf5ad1026add5f69f72ff4dc655bce310807a4a8
wettestsock <evilsharkeater@gmail.com>
2023-11-08 14:05:28 -0500

    sdkflk

--- hash-table/chash.c
+++ hash-table/chash.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define TABLE_SIZE 10
+#define STR_SIZE 256
 
 /*
 HASH TABLEEEEESS
@@ -22,16 +24,52 @@ each element has index
 */
 
 typedef struct {
-    char name[256];
+    char name[STR_SIZE];
     int age;
 }person;
 
-unsigned int hash(char* name) {
 
-    return 5;
+
+
+bool init_hash() { //initializes and sets all values to null
+    for (int i =0; i< TABLE_SIZE; i++){
+        hash_table[i] = NULL;
+    }
+
+}
+
+//want different names to return different index
+unsigned int hash(char* name) {
+    int length = strnlen(name, STR_SIZE);
+    unsigned int hash_val = 0;
+    for (int i =0; i<length; i++){
+        hash_val += name[i];
+        hash_val = (hash_val * name[i]) % TABLE_SIZE; 
+    } //okay hash function , limits it to 1 through 10
+    //ideal hash function will map each element to unique location
+
+    return hash_val;
+}
+
+void print_table() {
+    
+    for(int i=0; i< TABLE_SIZE; i++){
+        printf("%i\t", i);
+
+        if(hash_table[i]) printf("%i", hash_table[i]);
+        else printf("NULL");
+
+        printf('\n');
+
+    }
 }
 
 int main(){
+
+    person* hash_table[TABLE_SIZE];  //array of pointers to the names
+    //easy to tell when spot in table is empty
+    init_hash();
+    print_table();
     printf("Jacob => %u\n", hash("Jacob"));
     printf("Thomsa => %u\n", hash("Thoma"));
     printf("Joe => %u\n", hash("fdkjs"));
